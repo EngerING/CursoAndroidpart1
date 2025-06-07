be4937cc7288b2997aeb79763ff1a92ac94ce207
EngerING <tortilladejamon75@gmail.com>
2025-06-07 17:20:10 -0400

    Add files via upload

---
+++ Ejercicio1HolaAndroid/ejerciciosbutton/src/main/java/com/example/ejercicio2button/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.ejercicio2button
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+
+
+
+            }
+}
+
+}
+
+
+
+
